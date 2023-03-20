ce4262d877f181c6190c539ac64ffa191051e1c5
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-03-20 09:48:09 +0300

    model: update - testimonialSchema

--- server/models/testimonials.ts
+++ server/models/testimonials.ts
@@ -5,14 +5,17 @@ const testimonialSchema = new Schema(
     customerImage: {
       type: String,
       required: false,
+      trim: true,
     },
     customerName: {
       type: String,
       required: true,
+      trim: true,
     },
     customerComment: {
       type: String,
       required: true,
+      trim: true,
     },
   },
   { collection: 'testimonials' },
