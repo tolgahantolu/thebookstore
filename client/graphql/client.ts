2fb93807dbff4f0d194918efd35adbc74bd125d3
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-04-17 07:11:38 +0300

    apollo server link added

--- client/graphql/client.ts
+++ client/graphql/client.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 export const apolloClient = () => {
   return new ApolloClient({
     link: new HttpLink({
-      uri: 'http://localhost:4000/',
+      uri: process.env.APOLLO_SERVER,
     }),
     cache: new InMemoryCache(),
     ssrMode: typeof window === 'undefined',
