3dfc03cf23add55c3504e0777ccf4a765e91730d
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-04-26 11:06:03 +0300

    footer responsive fix and pagination bullets fix

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -3,9 +3,9 @@ import { SiUps, SiFedex, SiVisa } from 'react-icons/si';
 
 const Footer = () => {
   return (
-    <div className="mt-20 px-4 xs:px-12 sm:px-20 md:px-[100px] text-theme-gray flex justify-between items-center pb-5">
+    <div className="mt-20 px-4 xs:px-12 sm:px-20 md:px-[100px] text-theme-gray flex flex-col xs:flex-row justify-center xs:justify-between items-center gap-2 xs:gap-0 pb-5">
       <p data-testid="copyright" className="text-base">
-        Demo 2023 © Tolgahan Tolu. All Rights Reserved
+        Demo 2023 © Tolgahan Tolu.
       </p>
       <div className="flex items-center justify-end gap-2">
         <span className="text-3xl hover:text-theme-black transition">
